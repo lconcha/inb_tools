9e68c410e1808e5faaf8df85e15e1cb489adca44
Luis Concha <lconcha@gmail.com>
2023-08-01 18:15:54 -0600

    no more cuda9

--- inb_config_cuda9.sh
+++ inb_config_cuda9.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "[INFO] CUDA 9.1 is not needed for fsl 6.0.7 and above."
+echo "[INFO] Not setting up CUDA 9.1, use CUDA 10.1 instead."
+echo "[INFO] If you _really_ need CUDA9.1, do:"
+echo "       cudaLib=/usr/local/cuda-9.1/lib64"
+echo "       export LD_LIBRARY_PATH=${cudaLib}:${LD_LIBRARY_PATH}"
+exit 0
+
+
 cudaLib=/usr/local/cuda-9.1/lib64
 
 if [ ! -d ${cudaLib} ]

8f1a9b29ff723ef227080b65516e6ef6f199ebe2
Luis Concha <lconcha@gmail.com>
2024-07-24 12:58:45 -0600

    removed ping test, should suffice with qhost

--- inb_cluster_NFS_status_simple.sh
+++ inb_cluster_NFS_status_simple.sh
@@ -32,7 +32,7 @@ skip=1
 
 
 verbosity=""
-tout=10;# seconds
+tout=20;# seconds
 for arg in "$@"
 do
   case "$arg" in
@@ -94,7 +94,7 @@ do
     then
       echo "  $hostNameShort is declared as unreachable by SGE, will not check."
       isW=1
-      break
+      continue
     fi
     if [ $isW -eq 1 ]
     then
@@ -102,26 +102,26 @@ do
     fi
   done
 
-  this_ping_OK=0
-  ping -c 1 -q $h > /dev/null && this_ping_OK=1
-  doWarn=1
-  if [ $this_ping_OK -eq 0 ]
-  then
-    for w in $whiteList
-    do
-      if [[ "$hostNameShort" == "$w" ]]
-      then
-        echo "  $hostNameShort is down but it is whitelisted"
-        doWarn=0
-        break
-      fi
-    done
-    if [ $doWarn -eq 1 ]
-    then
-      echo -e "ERROR: $h is DOWN!"
-    fi
-    continue
-  fi
+  this_ping_OK=1
+  # ping -c 1 -q $h > /dev/null && this_ping_OK=1
+  # doWarn=1
+  # if [ $this_ping_OK -eq 0 ]
+  # then
+  #   for w in $whiteList
+  #   do
+  #     if [[ "$hostNameShort" == "$w" ]]
+  #     then
+  #       echo "  $hostNameShort is down but it is whitelisted"
+  #       doWarn=0
+  #       break
+  #     fi
+  #   done
+  #   if [ $doWarn -eq 1 ]
+  #   then
+  #     echo -e "ERROR: $h is DOWN!"
+  #   fi
+  #   continue
+  # fi
 
 
   ######################

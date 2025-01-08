d0479d505f8d0b43e22c0f99ff340490fcf25ff0
Luis Concha <lconcha@gmail.com>
2025-01-08 14:13:26 -0600

    add epsilon to make sure we always have a fixel. Also, change variable name to METRIC to make it more general

--- inb_mrds_scalePDDs.sh
+++ inb_mrds_scalePDDs.sh
@@ -2,17 +2,16 @@
 
 
 PDDs=$1
-COMPSIZES=$2
+METRIC=$2; # usually COMP_SIZE, but it can also be FA or MD.
 scaled_PDDs=$3
 
 tmpDir=$(mktemp -d)
 
-# mrconvert -quiet -coord 3 0:2 $PDDs ${tmpDir}/PDD_0.mif
-# mrconvert -quiet -coord 3 3:5 $PDDs ${tmpDir}/PDD_1.mif
-# mrconvert -quiet -coord 3 6:8 $PDDs ${tmpDir}/PDD_2.mif
+
+epsilon=0.000001; # to make sure we always have a fixel even when the metric to scale by is zero.
 
 
-nComponents=$(mrinfo -size $COMPSIZES | awk '{print $4}')
+nComponents=$(mrinfo -size $METRIC | awk '{print $4}')
 echolor green "[INFO] Max number of components per voxel is $nComponents"
 
 for c in $(seq 0 $(($nComponents -1)))
@@ -21,21 +20,14 @@ do
   j=$(mrcalc $i 2 -add)
   echolor green "[INFO] Tensor $c, volumes $i to $j"
   mrconvert -quiet -coord 3 $i:$j $PDDs ${tmpDir}/PDD_${c}.mif
-  mrconvert -quiet -coord 3 $c $COMPSIZES ${tmpDir}/fraction_${c}.mif
+  mrconvert -quiet -coord 3 $c $METRIC ${tmpDir}/fraction_${c}.mif
   mrcalc -quiet ${tmpDir}/PDD_${c}.mif \
     ${tmpDir}/fraction_${c}.mif \
     -mul \
+    $epsilon -add \
     ${tmpDir}/scaled_PDD_${c}.mif
 done
 
-
-# mrconvert -quiet -coord 3 0 $COMPSIZES ${tmpDir}/fraction_0.mif
-# mrconvert -quiet -coord 3 1 $COMPSIZES ${tmpDir}/fraction_1.mif
-# mrconvert -quiet -coord 3 2 $COMPSIZES ${tmpDir}/fraction_2.mif
-
-# mrcalc ${tmpDir}/PDD_0.mif ${tmpDir}/fraction_0.mif -mul ${tmpDir}/scaled_PDD_0.mif
-# mrcalc ${tmpDir}/PDD_1.mif ${tmpDir}/fraction_1.mif -mul ${tmpDir}/scaled_PDD_1.mif
-# mrcalc ${tmpDir}/PDD_2.mif ${tmpDir}/fraction_2.mif -mul ${tmpDir}/scaled_PDD_2.mif
 
 
 mrcat -quiet -axis 3 ${tmpDir}/scaled_PDD_*.mif $scaled_PDDs

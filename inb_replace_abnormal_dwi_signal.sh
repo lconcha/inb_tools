65363488cd953125f76267a7890350d2eb0abc4e
Luis Concha <lconcha@gmail.com>
2025-05-08 08:14:47 -0600

    changed threshold

--- inb_replace_abnormal_dwi_signal.sh
+++ inb_replace_abnormal_dwi_signal.sh
@@ -12,12 +12,12 @@ bzeros=${tmpDir}/bzeros.mif
 dwi_only=${tmpDir}/dwi_only.mif
 dwiextract -bzero    $dwi         - | mrcalc - $mask -mul $bzeros
 dwiextract -no_bzero $dwi         - | mrcalc - $mask -mul $dwi_only
-mrmath     -axis 3   $bzeros mean - | mrcalc - $mask -mul $avb0
+mrmath     -axis 3   $bzeros max  - | mrcalc - $mask -mul $avb0
 
 
 mrcalc $dwi_only                $avb0     -gt ${tmpDir}/badvoxels.mif
 mrcalc ${tmpDir}/badvoxels.mif  0         -eq  ${tmpDir}/goodvoxels.mif
-mrcalc ${tmpDir}/badvoxels.mif  $avb0     -mul ${tmpDir}/badvoxels_with_b0signal.mif
+mrcalc ${tmpDir}/badvoxels.mif  $avb0 0.5 -mul -mul ${tmpDir}/badvoxels_with_b0signal.mif
 mrcalc ${tmpDir}/goodvoxels.mif $dwi_only -mul ${tmpDir}/goodvoxels_with_origsignal.mif
 mrcalc ${tmpDir}/badvoxels_with_b0signal.mif \
        ${tmpDir}/goodvoxels_with_origsignal.mif \
